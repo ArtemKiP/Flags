9a3027dae3c01f904896b09339fc586dab8d4936
ArtemKiP <der.pap@mail.ru>
2024-06-01 14:12:23 +0600

    First and the last commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from './Container';
+
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background-color: var(--colors-ui-base);
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
+
+const Title = styled(Link).attrs({
+    to: '/',
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
+
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  text-transform: capitalize;
+`;
+
+export const Header = () => {
+    const [theme, setTheme] = useState('light');
+
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    return (
+        <HeaderEl>
+            <Container>
+                <Wrapper>
+                    <Title>Где Мир?</Title>
+                </Wrapper>
+            </Container>
+        </HeaderEl>
+    );
+};
